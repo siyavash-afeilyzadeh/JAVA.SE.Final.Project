a124fcdb74549493172cbe35f75a21694c1823cf
siyavash-afeilyzadeh <siyavash.afeilyzadeh@gmail.com>
2026-07-30 23:05:12 +0330

    init Room DataAccess class

---
+++ src/main/java/black/model/da/RoomDA.java
@@ -0,0 +1,100 @@
+package black.model.da;
+
+import black.model.entity.Room;
+import black.model.entity.enums.RoomClass;
+import black.utils.ConnectionProvider;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class RoomDA implements AutoCloseable {
+    private ConnectionProvider connectionProvider = new ConnectionProvider();
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+
+    public void save(Room room) throws Exception{
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement(
+                "select ROOM_SEQ.nextval as NEXT_ID from DUAL"
+        );
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        room.setId(resultSet.getInt("NEXT_ID"));
+
+        preparedStatement = connection.prepareStatement(
+                "INSERT INTO ROOMS(ID, ROOM_NUMBER, PRICE_PER_NIGHT, ROOM_CAPACITY,ROOM_CLASS) VALUES(?, ?, ?, ?, ?)"
+        );
+        preparedStatement.setInt(1, room.getId());
+        preparedStatement.setInt(2, room.getRoomNumber());
+        preparedStatement.setBigDecimal(3, room.getPricePerNight());
+        preparedStatement.setInt(4, room.getRoomCapacity());
+        preparedStatement.setString(5,room.getRoomClass().name());
+        preparedStatement.execute();
+    }
+    public void update(Room room) throws Exception{
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement(
+                "UPDATE ROOMS SET ROOM_NUMBER=?, PRICE_PER_NIGHT=?, ROOM_CAPACITY=?, ROOM_CLASS=? WHERE ID=?"
+        );
+        preparedStatement.setInt(1, room.getRoomNumber());
+        preparedStatement.setBigDecimal(2, room.getPricePerNight());
+        preparedStatement.setInt(3, room.getRoomCapacity());
+        preparedStatement.setString(4, room.getRoomClass().name());
+        preparedStatement.setInt(5, room.getId());
+    }
+    public void delete(int id) throws Exception{
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement(
+                "DELETE FROM ROOMS WHERE ID=?"
+        );
+        preparedStatement.setInt(1, id);
+        preparedStatement.execute();
+    }
+
+    public Room findByID(int id) throws Exception{
+        Room room = null;
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement("SELECT * FROM ROOMS WHERE ID=?");
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        if(resultSet.next()){
+            room = Room
+                    .builder()
+                    .id(resultSet.getInt("id"))
+                    .roomNumber(resultSet.getInt("roomNumber"))
+                    .pricePerNight(resultSet.getBigDecimal("pricePerNight"))
+                    .roomNumber(resultSet.getInt("roomNumber"))
+                    .roomClass(RoomClass.valueOf(resultSet.getString("roomClass")))
+                    .build();
+        }
+        return room;
+    }
+
+    public Room findByRoomNumber (int roomNumber) throws Exception{
+        Room room = null;
+        connection = connectionProvider.getConnection();
+        preparedStatement = connection.prepareStatement("SELECT * FROM ROOMS WHERE ROOM_NUMBER=?");
+        preparedStatement.setInt(1, roomNumber);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if(resultSet.next()){
+            room = Room
+                    .builder()
+                    .id(resultSet.getInt("id"))
+                    .roomNumber(resultSet.getInt("roomNumber"))
+                    .pricePerNight(resultSet.getBigDecimal("pricePerNight"))
+                    .roomNumber(resultSet.getInt("roomNumber"))
+                    .roomCapacity(resultSet.getInt("roomCapacity"))
+                    .roomClass(RoomClass.valueOf(resultSet.getString("roomClass")))
+                    .build();
+        }
+        return room;
+    }
+
+    @Override
+    public void close() throws Exception{
+        preparedStatement.close();
+        connection.close();
+    }
+
+}
